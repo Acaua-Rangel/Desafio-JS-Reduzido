52c15cfd816d8e5f2cbaaa5ad884d84d7b431c7d
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2025-03-26 17:09:51 -0300

    Add Mask

--- js/contato.js
+++ js/contato.js
@@ -9,6 +9,9 @@ class Data {
         this.updateValues();
 
         document.getElementById("btnSubmit").onclick = (event) => this.updateValuesBtn(event);
+
+        document.getElementsByName("cpf")[0].addEventListener("input", this.applyMaskCPF);
+        document.getElementsByName("telefone")[0].addEventListener("input", this.applyMaskTel);
     }
 
     updateValuesBtn(event) {
@@ -37,6 +40,21 @@ class Data {
             "motivo": this.motivo, 
             "mensagem": this.mensagem,
         };
+    }
+
+    applyMaskCPF(event) {
+        let cpf = event.target.value.replace(/\D/g, "");
+        cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+        cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+        cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+        event.target.value = cpf;
+    }
+    
+    applyMaskTel(event) {
+        let telefone = event.target.value.replace(/\D/g, "");
+        telefone = telefone.replace(/(\d{2})(\d)/, "($1) $2");
+        telefone = telefone.replace(/(\d{5})(\d{1,4})$/, "$1-$2");
+        event.target.value = telefone;
     }
 
 }
